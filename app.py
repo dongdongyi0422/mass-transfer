7cc11e86ade8350d2a348b835b4494e8debf1be9
dongdongyi0422 <redkeo02@gmail.com>
2025-09-15 23:21:34 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -304,18 +304,18 @@ with colB:
     leg.get_frame().set_alpha(0.85); leg.get_frame().set_facecolor("white")
     st.pyplot(figBand, use_container_width=True); plt.close(figBand)
 
-st.subheader("Permeance (SI)")
-fig1, ax1 = plt.subplots(figsize=(9,3))
+    st.subheader("Permeance (SI)")
+    fig1, ax1 = plt.subplots(figsize=(9,3))
 
-GPU = 1/3.35e-10  # 환산계수
+    GPU = 1/3.35e-10  # 환산계수
 
-ax1.plot(relP, Pi1, label=f"{gas1} (~{Pi1.max()*GPU:,.0f} GPU)")
-ax1.plot(relP, Pi2, '--', label=f"{gas2} (~{Pi2.max()*GPU:,.0f} GPU)")
+    ax1.plot(relP, Pi1, label=f"{gas1} (~{Pi1.max()*GPU:,.0f} GPU)")
+    ax1.plot(relP, Pi2, '--', label=f"{gas2} (~{Pi2.max()*GPU:,.0f} GPU)")
 
-ax1.set_ylabel(r"$\Pi$  (mol m$^{-2}$ s$^{-1}$ Pa$^{-1}$)")
-ax1.set_xlabel(r"Relative pressure, $P/P_0$ (–)")
-ax1.grid(True); ax1.legend()
-st.pyplot(fig1, use_container_width=True); plt.close(fig1)
+    ax1.set_ylabel(r"$\Pi$  (mol m$^{-2}$ s$^{-1}$ Pa$^{-1}$)")
+    ax1.set_xlabel(r"Relative pressure, $P/P_0$ (–)")
+    ax1.grid(True); ax1.legend()
+    st.pyplot(fig1, use_container_width=True); plt.close(fig1)
 
 
     st.subheader("Selectivity")
